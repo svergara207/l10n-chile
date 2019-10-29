a3f28d49bfc64c9c9f3f9aeeb9edc49ba60254ab
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-10-29 08:43:35 -0600

    [ADD] l10n_cl_dte_stock

---
+++ l10n_cl_dte_stock/models/res_company_document.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2019 Konos
+# Copyright (C) 2019 Blanco Martín & Asociados
+# Copyright (C) 2019 CubicERP
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+class ResCompanyDocument(models.Model):
+    _inherit = 'res.company.document'
+
+    name = fields.Selection(selection_add=[('picking', 'Picking')])
