12833a8d3846e47670b1826394bde7cc05700a8e
Sammy Vergara <savergar@ccu.cl>
2021-02-04 18:21:24 -0300

    new guias sii chile

--- l10n_cl_etd_stock/models/stock_picking.py
+++ l10n_cl_etd_stock/models/stock_picking.py
@@ -36,3 +36,32 @@ class StockPicking(models.Model):
                      ('document_type', '=', 'stock_picking')], limit=1)
                 vals.update({'class_id': sii_document and sii_document.id})
         return super(StockPicking, self).create(vals)
+
+    transport_type = fields.Selection(
+        [
+            ('2', 'Despacho por cuenta de empresa'),
+            ('1', 'Despacho por cuenta del cliente'),
+            ('3', 'Despacho Externo'),
+            ('0', 'Sin Definir')
+        ],
+        string="Tipo de Despacho",
+        default="2",
+        readonly=False, states={'done': [('readonly', True)]},
+    )
+
+    move_reason = fields.Selection(
+        [
+            ('1', 'Operación constituye venta'),
+            ('2', 'Ventas por efectuar'),
+            ('3', 'Consignaciones'),
+            ('4', 'Entrega Gratuita'),
+            ('5', 'Traslados Internos'),
+            ('6', 'Otros traslados no venta'),
+            ('7', 'Guía de Devolución'),
+            ('8', 'Traslado para exportación'),
+            ('9', 'Ventas para exportación')
+        ],
+        string='Razón del traslado',
+        default="2",
+        readonly=False, states={'done': [('readonly', False)]},
+    )    
